cd3e8d3228feb93ccc4771a875b72591ae1e93bf
lirian shabani <liriansh1@gmail.com>
2021-05-04 13:52:52 +0200

    added wtf-forms submit()

--- library/forms.py
+++ library/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField, SubmitField
 from wtforms.validators import InputRequired, Email, Length, email_validator
 
 
@@ -7,17 +7,23 @@ from wtforms.validators import InputRequired, Email, Length, email_validator
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=18)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=50)])
-    remember_me = BooleanField('remember me')
+    submit = SubmitField('Log In')
+    
     
 class RegisterForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=18)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=50)])
+    submit = SubmitField('Register')
     
 
 class BookForm(FlaskForm):
-    book_author = StringField('book_author',validators=[InputRequired(), Length(min=3, max=25)] )
     title = StringField('title',validators=[InputRequired(), Length(min=3, max=25)])
     description = TextAreaField('textarea', validators=[InputRequired(), Length(min=25, max=250)])
+    submit = SubmitField('Add Book')
+    
+class CommentForm(FlaskForm):
+    comment_text = StringField('comment_text',validators=[InputRequired(), Length(min=3, max=25)])
+    submit = SubmitField('Add Comment')
     
     
